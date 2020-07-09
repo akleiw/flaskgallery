c31efcfa0dbcd879d3f4c310f8a7123d60198197
akleiw <mkpilarski@gmail.com>
2020-07-09 17:35:51 +0000

    first try at sqlalchemy

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,23 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="pilarski",
+    password="passformysql",
+    hostname="pilarski.mysql.pythonanywhere-services.com",
+    databasename="pilarski$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
