a2b23c286898434385e69d726a575fe9c2048967
akleiw <mkpilarski@gmail.com>
2020-08-02 11:15:40 +0200

    introduced .env file for config

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-        username="pilarski",
-        password="passformysql",
-        hostname="pilarski.mysql.pythonanywhere-services.com",
-        databasename="pilarski$comments",
-    )
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    #     username="pilarski",
+    #     password="passformysql",
+    #     hostname="pilarski.mysql.pythonanywhere-services.com",
+    #     databasename="pilarski$comments",
+    # )
 
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
