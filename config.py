f08f0af6df5f4075d942037294727b6863d47a98
akleiw <mkpilarski@gmail.com>
2020-08-02 11:30:58 +0200

    removed commented legacy uri from config.py

--- config.py
+++ config.py
@@ -8,12 +8,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    #     username="pilarski",
-    #     password="passformysql",
-    #     hostname="pilarski.mysql.pythonanywhere-services.com",
-    #     databasename="pilarski$comments",
-    # )
 
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
