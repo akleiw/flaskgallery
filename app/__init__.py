1aa8862aea66bf0f9eb50e2b7839c573af5fcde6
akleiw <mkpilarski@gmail.com>
2020-08-07 21:41:54 +0000

    added caching to get_albums function

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_caching import Cache
 from gphotospy import authorize
 from config import Config
 import os
@@ -11,6 +12,7 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+cache = Cache(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
