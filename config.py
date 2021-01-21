2c4dcebe226ea6b95271dc200bab3bb4a806857d
akleiw <mkpilarski@gmail.com>
2021-01-21 22:23:17 +0100

    added sqlite as the default database fallback (app.db)

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
