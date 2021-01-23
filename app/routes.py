6ad1e23f2bdf6a7e0c5b914c4b592f2ecb18cbc2
akleiw <mkpilarski@gmail.com>
2021-01-23 19:17:21 +0000

    Fixed a bug - passing wrong argument to login_user

--- app/routes.py
+++ app/routes.py
@@ -60,7 +60,7 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user.id, remember=form.remember_me.data)
+        login_user(user, remember=form.remember_me.data)
         return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
 
