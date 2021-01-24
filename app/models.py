4db39608e7c2ef050ed3d440b507aaa935288cbc
akleiw <mkpilarski@gmail.com>
2021-01-24 17:40:15 +0100

    first try at db joing

--- app/models.py
+++ app/models.py
@@ -11,6 +11,13 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+users_groups = db.Table(
+    'users_groups',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id'))
+)
+
+
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
@@ -24,16 +31,24 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    groups = db.relationship(
+        'Group', secondary=users_groups,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 
-class Comment(db.Model):
-
-    __tablename__ = "comments"
-
+class Album(db.Model):
+    __tablename__ = "albums"
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
-    posted = db.Column(db.DateTime, default=datetime.now)
-    commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
-    commenter = db.relationship('User', foreign_keys=commenter_id)
+    gphotos_id = db.Column(db.String(100), index=True)
+
+
+class Group(db.Model):
+    """docstring for UserGroup"""
+    __tablename__ = "groups"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True)
