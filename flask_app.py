1a3d2677f13b0fb864826a101668829dbbfd6707
akleiw <mkpilarski@gmail.com>
2021-02-08 19:34:29 +0000

    renamed Group to Role (continued)

--- flask_app.py
+++ flask_app.py
@@ -2,12 +2,12 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from app import app, db
-from app.models import User, Group, Album
+from app.models import User, Role, Album
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Group' : Group, 'Album' : Album}
+    return {'db': db, 'User': User, 'Role' : Role, 'Album' : Album}
 
 
 if __name__ == '__main__':
