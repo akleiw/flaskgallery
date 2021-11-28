2468d19d88d340dacd49a887a4584872e89c6fa7
akleiw <mkpilarski@gmail.com>
2021-11-28 20:40:33 +0100

    pyLint, black, isort

--- app/gphotos.py
+++ app/gphotos.py
@@ -1,24 +1,26 @@
-import unicodedata
-import string
-import urllib.request
+# pylint: disable=no-member
 import os
-from sqlalchemy import delete
+import string
+import unicodedata
+import urllib.request
+
 from flask_login import current_user
-from app import service, cache
-from app import app, db
-from app.models import Album, Role
 from gphotospy.album import Album as GPhotosAlbum
 from gphotospy.media import Media, MediaItem
+from sqlalchemy import delete
+
+from app import app, cache, db, service
+from app.models import Album, Role
 
 
 def normalize_for_url(text: str):
     # spaces and slashes to underscores, all other punctuation
-    translation = str.maketrans(' /', '__', string.punctuation)
-    text = unicodedata.normalize('NFD', text).encode(
-        'ascii', 'ignore').decode('utf-8')  # replace special characters with their unicode counterparts
+    translation = str.maketrans(" /", "__", string.punctuation)
+    text = (
+        unicodedata.normalize("NFD", text).encode("ascii", "ignore").decode("utf-8")
+    )  # replace special characters with their unicode counterparts
     text = text.translate(translation)
     return text
-
 
 
 def get_albums():
@@ -28,31 +30,35 @@ def get_albums():
         query = current_user.albums()
     return {a.url_title: a for a in query}
 
+
 def cache_albums(refresh_thumbnails=False):
     album_manager = GPhotosAlbum(service)
     current_ids = list()
     for a in album_manager.list():
-        album = Album.query.filter_by(gphotos_id = a.get('id')).first()
+        album = Album.query.filter_by(gphotos_id=a.get("id")).first()
         if not album:
             album = Album()
-        album.gphotos_id = a.get('id')
-        current_ids.append(a.get('id'))
-        album.title = a.get('title')
-        album.url_title = normalize_for_url(a.get('title'))
-        album.items_count = a.get('mediaItemsCount')
+        album.gphotos_id = a.get("id")
+        current_ids.append(a.get("id"))
+        album.title = a.get("title")
+        album.url_title = normalize_for_url(a.get("title"))
+        album.items_count = a.get("mediaItemsCount")
         db.session.add(album)
-        thumbnail = os.path.join(app.config['ALBUM_THUMB_PATH'], a.get('id') + '.jpg')
+        thumbnail = os.path.join(app.config["ALBUM_THUMB_PATH"], a.get("id") + ".jpg")
         if not os.path.exists(thumbnail) or refresh_thumbnails:
-            urllib.request.urlretrieve(a.get('coverPhotoBaseUrl')+ "=w300-h200-c", os.path.join(app.config['ALBUM_THUMB_PATH'], a.get('id') + '.jpg'))
+            urllib.request.urlretrieve(
+                a.get("coverPhotoBaseUrl") + "=w300-h200-c",
+                os.path.join(app.config["ALBUM_THUMB_PATH"], a.get("id") + ".jpg"),
+            )
 
     # delete from db albums no longer in google photos
     stmt = delete(Album).where(Album.gphotos_id.notin_(current_ids)).execution_options(synchronize_session="fetch")
     db.session.execute(stmt)
     db.session.commit()
 
+
 @cache.memoize()
 def get_media(album_id):
     media_manager = Media(service)
-    album_media_list = list(MediaItem(m)
-                            for m in media_manager.search_album(album_id))
+    album_media_list = list(MediaItem(m) for m in media_manager.search_album(album_id))
     return album_media_list
--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+# pylint: disable=no-member
 import os
+from datetime import datetime
 
-from app import db, login_manager, app
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import app, db, login_manager
 
 
 @login_manager.user_loader
@@ -12,19 +14,19 @@ def load_user(id):
 
 
 users_roles = db.Table(
-    'users_roles',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
+    "users_roles",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("role_id", db.Integer, db.ForeignKey("roles.id")),
 )
 
 albums_roles = db.Table(
-    'albums_roles',
-    db.Column('album_id', db.Integer, db.ForeignKey('albums.id')),
-    db.Column('role_id', db.Integer, db.ForeignKey('roles.id'))
+    "albums_roles",
+    db.Column("album_id", db.Integer, db.ForeignKey("albums.id")),
+    db.Column("role_id", db.Integer, db.ForeignKey("roles.id")),
 )
 
 
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model):  # type: ignore
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
@@ -37,58 +39,54 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    roles = db.relationship(
-        'Role', secondary=users_roles,
-        backref=db.backref('users', lazy='dynamic'),
-        lazy='dynamic')
+    roles = db.relationship("Role", secondary=users_roles, backref=db.backref("users", lazy="dynamic"), lazy="dynamic")
 
     def albums(self):
-        if Role.get_admin_role() in self.roles:
+        if Role.get_admin_role() in self.roles:  # pylint: disable=unsupported-membership-test
             return Album.query
-        return Album.query.join(albums_roles, (albums_roles.c.album_id == Album.id)).join(
-            users_roles, (users_roles.c.role_id == albums_roles.c.role_id)).filter(
-                users_roles.c.user_id == self.id)
-
+        return (
+            Album.query.join(albums_roles, (albums_roles.c.album_id == Album.id))
+            .join(users_roles, (users_roles.c.role_id == albums_roles.c.role_id))
+            .filter(users_roles.c.user_id == self.id)
+        )
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return "<User {}>".format(self.username)
 
 
-class Album(db.Model):
+class Album(db.Model):  # type: ignore
     __tablename__ = "albums"
     id = db.Column(db.Integer, primary_key=True)
     gphotos_id = db.Column(db.String(100), index=True)
     title = db.Column(db.String, index=True)
     url_title = db.Column(db.String, index=True)
     items_count = db.Column(db.Integer)
-    
+
     def thumbnail_url(self):
-        return os.path.join(app.static_url_path, app.config.get('THUMBNAIL_FOLDER'), self.gphotos_id + '.jpg')
+        return os.path.join(app.static_url_path, app.config.get("THUMBNAIL_FOLDER"), self.gphotos_id + ".jpg")
 
     def __repr__(self):
-        return '<Album {}>'.format(self.title)
+        return "<Album {}>".format(self.title)
 
 
-
-class Role(db.Model):
+class Role(db.Model):  # type: ignore
     """docstring for Userrole"""
+
     __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True)
 
     albums = db.relationship(
-        'Album', secondary=albums_roles,
-        backref=db.backref('roles', lazy='dynamic'),
-        lazy='dynamic')
+        "Album", secondary=albums_roles, backref=db.backref("roles", lazy="dynamic"), lazy="dynamic"
+    )
 
     def __repr__(self):
-        return '<Role {}>'.format(self.name)
+        return "<Role {}>".format(self.name)
 
     @classmethod
     def get_public_role(cls):
-        return cls.query.filter_by(name = 'public').first()
+        return cls.query.filter_by(name="public").first()
 
     @classmethod
     def get_admin_role(cls):
-        return cls.query.filter_by(name = 'admin').first()
-
+        return cls.query.filter_by(name="admin").first()
