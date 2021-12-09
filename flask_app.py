1c5372e8f0e5d5db54e3c5f4e1dada1642819ced
Marek Pilarski <mp@aiclearing.com>
2021-12-09 21:12:12 +0100

    added command assing-role

--- flask_app.py
+++ flask_app.py
@@ -1,25 +1,33 @@
-
-
 import click
+
 from app import app, db
-from app.models import User, Role, Album
+from app.models import Album, Role, User
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Role': Role, 'Album': Album}
+    return {"db": db, "User": User, "Role": Role, "Album": Album}
 
 
 @app.cli.command("create-admin")
 @click.argument("password")
 def create_admin(password):
-    admin_role = Role.query.filter_by(name='admin').first()
-    admin = admin_role.users.first() or User(username='admin')
+    admin_role = Role.query.filter_by(name="admin").first()
+    admin = admin_role.users.first() or User(username="admin")
     admin.set_password(password)
     admin_role.users.append(admin)
     db.session.add(admin)
     db.session.commit()
 
 
-if __name__ == '__main__':
+@app.cli.command("assign-role")
+@click.argument("role_name")
+def assign_role(role_name):
+    """Assigns given role to all albums in gallery"""
+    role = Role.query.filter_by(name=role_name).first()
+    role.albums = Album.query.all()
+    db.session.commit()
+
+
+if __name__ == "__main__":
     app.run()
